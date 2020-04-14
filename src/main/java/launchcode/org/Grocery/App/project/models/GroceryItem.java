690f0b95b7571ab91dc46013244ae1009a6b3a09
Rachelle Kuhl <alightinmyhair@gmail.com>
2020-04-14 18:42:51 -0500

    Added id field to GroceryItem obj and created a equals() and hashcode() method

--- src/main/java/launchcode/org/Grocery/App/project/models/GroceryItem.java
+++ src/main/java/launchcode/org/Grocery/App/project/models/GroceryItem.java
@@ -1,6 +1,11 @@
 package launchcode.org.Grocery.App.project.models;
 
+import java.util.Objects;
+
 public class GroceryItem {
+
+    private int id;
+    private static int nextId=1;
 
     private String name;
     private String category;
@@ -8,6 +13,8 @@ public class GroceryItem {
     public GroceryItem(String name, String category) {
         this.name = name;
         this.category = category;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -26,9 +33,26 @@ public class GroceryItem {
         this.category = category;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroceryItem that = (GroceryItem) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
