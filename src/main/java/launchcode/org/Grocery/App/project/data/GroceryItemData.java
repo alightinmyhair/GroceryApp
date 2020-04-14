9da2a2fc23ae2ea46d67fabbff98c682694cb48d
Rachelle Kuhl <alightinmyhair@gmail.com>
2020-04-14 18:57:50 -0500

    Started Creating a Data Layer

---
+++ src/main/java/launchcode/org/Grocery/App/project/data/GroceryItemData.java
@@ -0,0 +1,15 @@
+package launchcode.org.Grocery.App.project.data;
+
+import launchcode.org.Grocery.App.project.models.GroceryItem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GroceryItemData {
+    //need a place to put our Grocery Items - this will be our main property
+    private static Map<Integer, GroceryItem> items = new HashMap<>();
+    //need to get all items
+    //need to get a single item
+    //add an item
+    //delete item
+}
