3a4a8ece6fd69d7bb4b21d3d8bb3228549252918
Rachelle Kuhl <alightinmyhair@gmail.com>
2020-05-03 16:23:35 -0500

    Refactored remove method handler

--- src/main/java/launchcode/org/Grocery/App/project/controllers/GroceryListController.java
+++ src/main/java/launchcode/org/Grocery/App/project/controllers/GroceryListController.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 public class GroceryListController {
 
     @GetMapping
-    public String displayGroceryList(Model model){
+    public String displayGroceryList(Model model) {
 
         model.addAttribute("items", GroceryItemData.getAll());
         model.addAttribute("categories", GroceryCategory.values());
@@ -26,17 +26,18 @@ public class GroceryListController {
         return "groceryList/index";
 
     }
+
     // display a form in each row on groceryList
     //loop through the grocery items in a table,
     //with each table row being a form
     //in the form my input fields would be my th field
     @GetMapping("/editgrocery")
-    public String displayEditForm(Model model, int[] itemIds, String edit){
+    public String displayEditForm(Model model, int[] itemIds, String edit) {
 
         //If edit button is clicked, the below happens
-        if (edit != null){
+        if (edit != null) {
             ArrayList<GroceryItem> groceryItems1 = new ArrayList<GroceryItem>();
-            for(int i = 0; i < itemIds.length; i++){
+            for (int i = 0; i < itemIds.length; i++) {
                 System.out.println(itemIds[i]);
                 groceryItems1.add(GroceryItemData.getById(itemIds[i]));
             }
@@ -54,7 +55,7 @@ public class GroceryListController {
     }
 
     @PostMapping("/editgrocery")
-    public String editGroceryItem(int id, String name, String description, GroceryCategory category, Model model){
+    public String editGroceryItem(int id, String name, String description, GroceryCategory category, Model model) {
 //
 //        if(GroceryItemData.getAll() != null){
 //            for (){
@@ -64,53 +65,61 @@ public class GroceryListController {
 //        }
 
         System.out.println("hi");
-        model.addAttribute("items",GroceryItemData.getAll());
+        model.addAttribute("items", GroceryItemData.getAll());
         model.addAttribute("categories", GroceryCategory.values());
 
         return "groceryList/index";
     }
 
     @PostMapping("/add")
-    public String addGroceryItem(@ModelAttribute @Valid GroceryItem newGroceryItem, Model model){
+    public String addGroceryItem(@ModelAttribute @Valid GroceryItem newGroceryItem, Model model) {
 
         GroceryItemData.add(newGroceryItem);
-        model.addAttribute("items",GroceryItemData.getAll());
+        model.addAttribute("items", GroceryItemData.getAll());
         model.addAttribute("categories", GroceryCategory.values());
 
         return "groceryList/index";
     }
 
-    @PostMapping("/remove")
-    public String removeGroceryItem(Model model, @RequestParam int[] itemIds, String delete, String edit){
+    @PostMapping("/update")
+    public String updateGroceryItem(Model model, @RequestParam int[] itemIds, String delete, String edit) {
 
         // TODO: RK - create new class to handle itemIds and edit/delete buttons?
 
+        if (edit != null) {
+            ArrayList<GroceryItem> groceryItems1 = new ArrayList<GroceryItem>();
+            for (int i = 0; i < itemIds.length; i++) {
+                System.out.println(itemIds[i]);
+                groceryItems1.add(GroceryItemData.getById(itemIds[i]));
+            }
 
-        if (edit != null){
-            System.out.println("edit button clicked");
-            System.out.println("hi");
-            //is this really getting the id?
-            model.addAttribute("items", GroceryItemData.getAll());
-            model.addAttribute("categories", GroceryCategory.values());
-//            GroceryItem groceryItem1 = GroceryItemData.getAll();
-//            System.out.println(groceryItem, groceryItem1);
-            return "groceryList/edit";
+            model.addAttribute("items", groceryItems1);
         }
+//        if (edit != null){
+//            System.out.println("edit button clicked");
+//
+//            GroceryItem groceryItems = GroceryItemData.getById(itemIds[0]);
+//            model.addAttribute("groceryItems",groceryItems);
+//            model.addAttribute("categories", GroceryCategory.values());
+//
+//            return "groceryList/edit";
+//        }
 
-        if (delete != null){
-            System.out.println("delete clicked!");
-            if(itemIds != null){
-                for (int id : itemIds){
-                    System.out.println(GroceryItemData.getById(id));
-                    GroceryItemData.remove(id);
+            if (delete != null) {
+                System.out.println("delete clicked!");
+                if (itemIds != null) {
+                    for (int id : itemIds) {
+                        System.out.println(GroceryItemData.getById(id));
+                        GroceryItemData.remove(id);
+                    }
                 }
             }
+
+            model.addAttribute("items", GroceryItemData.getAll());
+            model.addAttribute("categories", GroceryCategory.values());
+            model.addAttribute(new GroceryItem());
+
+            return "groceryList/index";
         }
-
-        model.addAttribute("items",GroceryItemData.getAll());
-        model.addAttribute("categories", GroceryCategory.values());
-        model.addAttribute(new GroceryItem());
-
-        return "groceryList/index";
     }
-}
+
