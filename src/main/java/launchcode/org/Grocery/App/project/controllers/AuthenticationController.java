32a76b6e68668b7b5825f9441831a433669be646
Rachelle Kuhl <alightinmyhair@gmail.com>
2020-08-01 18:45:42 -0500

    Updates to Authentication Controller

--- src/main/java/launchcode/org/Grocery/App/project/controllers/AuthenticationController.java
+++ src/main/java/launchcode/org/Grocery/App/project/controllers/AuthenticationController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -139,6 +140,6 @@ public class AuthenticationController {
     @GetMapping("/logout")
     public String logout(HttpServletRequest request){
         request.getSession().invalidate();
-        return "redirect:/login";
+        return "login";
     }
 }
