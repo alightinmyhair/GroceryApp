6de5f2fa6ae4ca5f3bf55730b7c75b200d00868b
Rachelle Kuhl <alightinmyhair@gmail.com>
2020-08-01 18:13:22 -0500

    Deleted dead code, rearranged /login

--- src/main/java/launchcode/org/Grocery/App/project/controllers/AuthenticationController.java
+++ src/main/java/launchcode/org/Grocery/App/project/controllers/AuthenticationController.java
@@ -14,7 +14,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
