1e2c29a80f2add49f124e01b05ed2a907e250284
Rachelle Kuhl <alightinmyhair@gmail.com>
2020-08-01 20:52:30 -0500

    Added style.css to whitelisting

---
+++ src/main/java/launchcode/org/Grocery/App/project/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package launchcode.org.Grocery.App.project.controllers;
+
+import launchcode.org.Grocery.App.project.models.dto.LoginFormDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class HomeController {
+
+    @GetMapping
+    public String index(Model model){
+
+        // Kris testing
+        model.addAttribute("loginFormDTO", new LoginFormDTO());
+        return "login";
+    }
+}
