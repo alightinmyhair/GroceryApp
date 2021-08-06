07240dd42356d1f193581ecb4d4ce7bb988e9ad9
Rachelle Kuhl <alightinmyhair@gmail.com>
2021-08-06 17:44:37 -0500

    Removed unused import

--- src/main/java/launchcode/org/Grocery/App/project/controllers/AuthenticationController.java
+++ src/main/java/launchcode/org/Grocery/App/project/controllers/AuthenticationController.java
@@ -14,9 +14,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
